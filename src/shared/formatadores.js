487f80b831c20da3eb4407f4dfef124dea27fe6b
Theo <theomarquesrx@gmail.com>
2024-05-18 18:02:40 -0300

    add formatadores

--- src/shared/formatadores.js
+++ src/shared/formatadores.js
@@ -14,3 +14,52 @@ export function normalizarData(date) {
 
   return returnDate;
 }
+
+export function formataCep(cep) {
+  if (!cep || cep.length < 8) {
+    return cep;
+  }
+  return [cep.slice(0, 5), "-", cep.slice(5)].join("");
+}
+
+export function formataCepRenderizacao(cep) {
+  const numericInput = cep.replace(/\D/g, "");
+  if (numericInput.length > 5) {
+    return `${numericInput.slice(0, 5)}-${numericInput.slice(5)}`;
+  } else {
+    return numericInput;
+  }
+}
+
+export function formataTelefone(telefone) {
+  if (telefone) {
+    const val = `(${telefone.ddd}) ${telefone.numTelefone}`;
+    var position = val.length - 4;
+    return [val.slice(0, position), "-", val.slice(position)].join("");
+  }
+  return null;
+}
+
+export function formatarFrase(frase) {
+  return frase
+    ? frase.charAt(0).toUpperCase() + frase.slice(1).toLowerCase()
+    : "-";
+}
+
+export function formataCPF(cpf) {
+  if (!cpf) {
+    return cpf;
+  }
+  return cpf
+    .replace(/[^\d]/g, "")
+    .padStart(11, "0")
+    .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+export function removeNaoNumericos(str) {
+  if (typeof str === "string") {
+    return str.replace(/\D/g, "");
+  } else {
+    return str;
+  }
+}
